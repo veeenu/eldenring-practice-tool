ac1da0cb6dd10b1551172d7da29f767a55e494c3
ViRazY <vinni5.2001@web.de>
2024-07-10 13:01:03 +0200

    Formatting.

--- practice-tool/src/widgets/label.rs
+++ practice-tool/src/widgets/label.rs
@@ -1,8 +1,6 @@
-use practice_tool_core::widgets::label::{LabelWidget};
+use practice_tool_core::widgets::label::LabelWidget;
 use practice_tool_core::widgets::Widget;
 
-pub(crate) fn label_widget(
-    label: &str,
-) -> Box<dyn Widget> {
+pub(crate) fn label_widget(label: &str) -> Box<dyn Widget> {
     Box::new(LabelWidget::new(label))
 }
